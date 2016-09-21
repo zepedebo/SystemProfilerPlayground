01fd2509679d3ad686b7ba5c86c0961d85f79144
Steve Goodrich <steven.goodrich@landesk.com>
2016-09-21 16:32:50 -0600

    Cleaned up the loops stuff

--- SystemProfilerPlayground.playground/Contents.swift
+++ SystemProfilerPlayground.playground/Contents.swift
@@ -56,24 +56,14 @@ firstTask.waitUntilExit()
 
 let lsData = stdout.fileHandleForReading.readDataToEndOfFile()
 if let lsText = String(data: lsData, encoding: String.Encoding.utf8) {
-//    print("\(lsText)")
     let contents = lsText.components(separatedBy: "\n")
-    let fullPaths = contents.map(){(a) -> String in firstTask.currentDirectoryPath+"/"+a}
+    let fullPaths = contents.map(){(a: String) -> String in firstTask.currentDirectoryPath+"/"+a}
+    print("\(fullPaths)")
     // let fullPaths = contents.map(){firstTask.currentDirectoryPath+"/"+$0}
-
-//    print("\(fullPaths)")
 }
 
-
-func toNSArray(v: Any?) -> NSArray? {
-    return v as? NSArray
-}
-
-func toNSDictionary(v: Any?) ->NSDictionary? {
-    return v as? NSDictionary
-}
-
-
+let q = String("2a3a5".characters.filter({$0 >= "0" && $0 <= "9"}))
+print("\(Int(q)!)")
 
 func getItemsFromSystemProfiler(dataTypeString: String) -> Array<NSDictionary>? {
     let task = Process()
@@ -92,7 +82,7 @@ func getItemsFromSystemProfiler(dataTypeString: String) -> Array<NSDictionary>? 
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
 
 
-    let dict: Any! = try? PropertyListSerialization.propertyList(from: data, options: [], format: nil)
+    let dict: Any = try? PropertyListSerialization.propertyList(from: data, options: [], format: nil)
 
 
     guard let a = dict as? NSArray else {
@@ -119,34 +109,31 @@ print("\(d?.count)")
 var fora = [String](repeating: "", count: d!.count)
 
 timeMe(label: "Traditional for loop"){
-//for swp in d! {
-for n in 0..<d!.count {
-    if let path = d?[n]["path"] {
-        let fullpath = "\(path)/Contents/Info.plist"
-        NSDictionary(contentsOfFile: fullpath)?["CFBundleIdentifier"]  ?? ""
+    for n in 0..<d!.count {
+        if let path = d?[n]["path"] {
+            let fullpath = "\(path)/Contents/Info.plist"
+            NSDictionary(contentsOfFile: fullpath)?["CFBundleIdentifier"]  ?? ""
+        }
     }
 }
+
+timeMe(label: "For in loop") {
+    var fora = [String]()
+    for swp in d! {
+        if let path = swp["path"] {
+            let fullpath = "\(path)/Contents/Info.plist"
+            if case let info as String = NSDictionary(contentsOfFile: fullpath)?["CFBundleIdentifier"] {
+                fora.append(info)
+    //            print("\(info)")
+            } else {
+                fora.append("")
+            }
+        }
+    }
 }
-//let fortimeAtStart = Date()
 
-//var fora = [String]()
-//for swp in d! {
-//    if let path = swp["path"] {
-//        let fullpath = "\(path)/Contents/Info.plist"
-//        if case let info as String = NSDictionary(contentsOfFile: fullpath)?["CFBundleIdentifier"] {
-//            fora.append(info)
-////            print("\(info)")
-//        } else {
-//            print(fullpath)
-//            fora.append("")
-//        }
-//    }
-//}
-// let forelapsedtime = Date().timeIntervalSince(fortimeAtStart)
-// print("\(forelapsedtime)")
 
-timeMe(label: "Enumerated for loop"){
-    //for swp in d! {
+timeMe(label: "Enumerated for loop pre allocated"){
     for (n, path) in (d?.enumerated())! {
         if let path = d?[n]["path"] {
             let fullpath = "\(path)/Contents/Info.plist"
@@ -162,17 +149,25 @@ timeMe(label: "Enumerated for loop"){
 
 var mapa: [String] = []
 timeMe(label: "Map") {mapa = d!.map{(swp) -> String in
-    if let path = swp["path"] {
-        let fullpath = "\(path)/Contents/Info.plist"
-        if let info = NSDictionary(contentsOfFile: fullpath)?["CFBundleIdentifier"] {
-//            print("\(info)")
-            return   info as! String
-        }
+        if let path = swp["path"] {
+            let fullpath = "\(path)/Contents/Info.plist"
+            if let info = NSDictionary(contentsOfFile: fullpath)?["CFBundleIdentifier"] {
+                return   info as! String
+            }
 
+        }
+        return ""
     }
-    return ""
 }
-}
+
+
+let add1 = {(a: Int) -> Int in return a + 1}
+let add2 = {a in return a + 2}
+let add3 = {$0 + 3}
+
+add1(10)
+add2(10)
+add3(10)
 
 //if mapa.count != fora.count {
 //    print("not equal \(mapa.count), \(fora.count)")
