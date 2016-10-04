c207ffc0863778e3f5eb636c95e68b5cdb780165
Steve Goodrich <steven.goodrich@landesk.com>
2016-10-04 11:08:01 -0600

    More examples

--- SystemProfilerPlayground.playground/Contents.swift
+++ SystemProfilerPlayground.playground/Contents.swift
@@ -10,6 +10,50 @@ func timeMe(label: String, code :(Void) -> Void ) {
 
 }
 
+extension Array {
+    public func pmap<T>(transform: @escaping ((Element) -> T)) -> [T] {
+        guard !self.isEmpty else {
+            return []
+        }
+        
+        var result: [(Int, [T])] = []
+        
+        let group = DispatchGroup()
+        let lock = DispatchQueue(label:"pmap queue for result" )// dispatch_queue_create("pmap queue for result", DISPATCH_QUEUE_SERIAL)
+        
+        let potentialStep = self.count / ProcessInfo.processInfo.activeProcessorCount
+        let step: Int = (potentialStep > 1) ? potentialStep : 1// step can never be 0
+        
+        //        for var stepIndex = 0; stepIndex * step < self.count; stepIndex += 1 {
+        var stepIndex = 0
+        while stepIndex * step < self.count {
+            let capturedStepIndex = stepIndex
+            
+            var stepResult: [T] = []
+            DispatchQueue.global().async(group: group) {
+                for i in (capturedStepIndex * step)..<((capturedStepIndex + 1) * step) {
+                    if i < self.count {
+                        let mappedElement = transform(self[i])
+                        stepResult += [mappedElement]
+                    }
+                }
+                
+                lock.async(group: group) {
+                    result += [(capturedStepIndex, stepResult)]
+                }
+            }
+            stepIndex += 1
+        }
+        group.wait()
+        
+        //        dispatch_group_wait(group, DispatchTime.distantFuture)
+        
+        return result.sorted { $0.0 < $1.0 }.flatMap { $0.1 }
+    }
+}
+
+
+
 // Basic task demo
 let lsTask = Process()
 let wcTask = Process()
@@ -148,7 +192,7 @@ timeMe(label: "Enumerated for loop pre allocated"){
 }
 
 var mapa: [String] = []
-timeMe(label: "Map") {mapa = d!.map{(swp) -> String in
+timeMe(label: "Map") {mapa = d!.pmap{(swp) -> String in
         if let path = swp["path"] {
             let fullpath = "\(path)/Contents/Info.plist"
             if let info = NSDictionary(contentsOfFile: fullpath)?["CFBundleIdentifier"] {
@@ -209,3 +253,9 @@ if case .some(let x) = so {
 } else {
     print("nan")
 }
+
+
+
+
+
+
